2f74e754fb25920472d6257fe90dcd045fc0baeb
tobi <113621482+Etomu@users.noreply.github.com>
2023-06-04 00:29:59 +0100

    Update 0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -25,9 +25,7 @@ def teardown_db(exception):
 
 @app.route('/0-hbnb/')
 def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
+    cache_id = str(uuid.uuid4())  # New line to generate a unique cache ID
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
@@ -39,7 +37,7 @@ def hbnb_filters(the_id=None):
                            amens=amens,
                            places=places,
                            users=users,
-                           cache_id=uuid.uuid4())
+                           cache_id=cache_id)
 
 if __name__ == "__main__":
     """
